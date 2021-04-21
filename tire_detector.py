f7308223e1707994fc56d42c0b465435f9f91c51
ivkis <i.kisialiou@gmail.com>
2021-04-21 12:37:02 +0100

    added video processing

--- tire_detector.py
+++ tire_detector.py
@@ -6,15 +6,23 @@ import imutils
 import time
 import os
 
-#python tire_detector.py -f car3.jpg -i image
+MODEL_PATH = 'model'  # folder name
 LABEL = 'tire'
-COLOR = (255, 0, 255) # purple
-MODEL_PATH = 'model' # folder name
+
+COLOR = (255, 0, 255)  # purple
+CAP_COLOR = (0, 0, 0)
+ALPHA = 0.5
+TEXT_SIZE = 0.7
+TEXT_THICKNESS = 2
+TEXT_FONT = cv2.FONT_HERSHEY_SIMPLEX
+TEXT_COLOR = (255, 255, 255)
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", required=True, help="path to input image or video")
 ap.add_argument("-i", "--input", required=True, help="type of input: 'image' or 'video'")
-ap.add_argument("-c", "--confidence", type=float, default=0.4, help="minimum probability to filter weak detections")
+#ap.add_argument("-m", "--model", required=True, help="yolo4' or 'yolo4tiny'")
+ap.add_argument("-c", "--confidence", type=float, default=0.25, help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppression")
 args = vars(ap.parse_args())
 
@@ -33,7 +41,7 @@ def draw_predictions(image):
     :param image:
     :return: image
     """
-    (H, W) = image.shape[:2] # input dimensions
+    (H, W) = image.shape[:2]  # input dimensions
     # construct a blob from the input image and then perform a forward
     # pass of the YOLO object detector, giving us our bounding boxes and
     # associated probabilities
@@ -43,7 +51,7 @@ def draw_predictions(image):
     outputs = model.forward(layer_names)
     end = time.time()
     # show timing information on YOLO
-    print("[INFO] YOLO inference took {:.6f} seconds".format(end - start))
+    infer_time = end - start
 
     # initialize our lists of detected bounding boxes, confidences, and
     # class IDs, respectively
@@ -52,55 +60,111 @@ def draw_predictions(image):
 
     # loop over outputs
     for output in outputs:
-    	# loop over each of the detections
-    	for detection in output:
-    		confidence = detection[5]
-    		# filter out predictions below the threshold
-    		if confidence > args["confidence"]:
-    			# scale the bounding box coordinates back
-    			box = detection[0:4] * np.array([W, H, W, H])
-    			(centerX, centerY, width, height) = box.astype("int")
-	    		# use the center (x, y)-coordinates to derive the top and
-	    		# and left corner of the bounding box
-	    		x = int(centerX - (width / 2))
-	    		y = int(centerY - (height / 2))
-	    		# update our list of bounding box coordinates, confidences,
-	    		# and class IDs
-	    		boxes.append([x, y, int(width), int(height)])
-	    		confidences.append(float(confidence))
+        # loop over each of the detections
+        for detection in output:
+            confidence = detection[5]
+            # filter out predictions below the threshold
+            if confidence > args["confidence"]:
+                # scale the bounding box coordinates back
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
 
     # apply non-maxima suppression to suppress weak, overlapping bounding boxes
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
 
     # MAKE THE OUTPUT IMAGE
-    if len(idxs) > 0:       # ensure at least one detection exists
-    	# loop over the indexes we are keeping
-	    for i in idxs.flatten():
-	    	# extract the bounding box coordinates
-	    	(x, y) = (boxes[i][0], boxes[i][1])
-	    	(w, h) = (boxes[i][2], boxes[i][3])
-	    	# draw a bounding box rectangle and label on the image
-	    	cv2.rectangle(image, (x, y), (x + w, y + h), COLOR, 2)
+    #caption_text = 'Model: {}'.format(model_name)
+    caption_text = ' No objects detected'
+    if len(idxs) > 0:  # ensure at least one detection exists
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
 
-	    	# text with background
-	    	text = "{:.1f} %".format(confidences[i] * 100)
-	    	# text = "{}: {:.2f}".format(LABEL, confidences[i])
-	    	(text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-	    	cv2.rectangle(image, (x - 1, y + 1),
-	    				  (x + max(w, text_width) + 1, y - text_height - baseline - 1),
-	    				  COLOR, -1)
-	    	cv2.putText(image, text,
-	    				(x, y - int(0.2 * text_height)),
-	    				cv2.FONT_HERSHEY_SIMPLEX,
-	    				0.5, (255, 255, 255), 1)
-    return image
+            # text with background
+            text = "{:.1f}%".format(confidences[i] * 100)
+            imboxes = image.copy()  # semi-opaque layer
+            cv2.rectangle(imboxes, (x, y), (x + w, y + h), COLOR, 4)
+
+            (text_width, text_height), text_baseline = cv2.getTextSize(text, TEXT_FONT, TEXT_SIZE, TEXT_THICKNESS)
+            cv2.rectangle(imboxes, (x - 2, y + h - 2),
+                          (x + max(w, text_width) + 2, y + h + text_height + text_baseline),
+                          COLOR, -1)
+            cv2.addWeighted(imboxes, ALPHA, image, 1-ALPHA, 0, image)
+            cv2.putText(image, text,
+                        (x, y + h + text_baseline + int(0.8 * text_height)),
+                        TEXT_FONT, TEXT_SIZE, TEXT_COLOR, TEXT_THICKNESS)
+        caption_text = " Inference time: {:.1f} ms".format(infer_time * 1000)
+    # Caption
+    imcaption = image.copy()
+    (text_width, text_height), text_baseline = cv2.getTextSize(caption_text, TEXT_FONT, TEXT_SIZE, TEXT_THICKNESS)
+    cv2.rectangle(imcaption, (0, 0), (W, text_height + 2 * text_baseline), CAP_COLOR, -1)
+    cv2.addWeighted(imcaption, ALPHA, image, 1 - ALPHA, 0, image)
+    cv2.putText(image, caption_text,
+                (0, text_height + text_baseline),
+                TEXT_FONT, TEXT_SIZE, TEXT_COLOR, TEXT_THICKNESS)
+        
+    return image, infer_time
+
 
 # LOAD INPUT
 if args["input"] == 'image':
+    # python tire_detector.py -f car3.jpg -i image
     image = cv2.imread(os.path.sep.join(["Input", args["file"]]))
-    image = draw_predictions(image)
+    image, infer_time = draw_predictions(image)
+    print("[INFO] YOLO inference took {:.6f} seconds".format(infer_time))
     cv2.imwrite('Output/{}'.format(args["file"]), image)
     cv2.imshow("Output", image)
     cv2.waitKey(0)
 
-#if args["input"] == 'video':
+elif args["input"] == 'video':
+    # python tire_detector.py -f video1.mp4 -i video
+    cap = cv2.VideoCapture(os.path.sep.join(["Input", args["file"]]))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    writer = None
+    (W, H) = (None, None)
+    # Estimation of the processing time
+    try:
+        prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+            else cv2.CAP_PROP_FRAME_COUNT
+        total = int(cap.get(prop))
+        print("[INFO] {} total frames in video".format(total))
+    except:
+        print("[INFO] could not determine # of frames in video")
+        print("[INFO] no approx. completion time can be provided")
+        total = -1
+    # Processing of the frames
+    while True:
+        (grabbed, frame) = cap.read()
+        if not grabbed: # end of the stream
+            break
+        # Object detection
+        frame, infer_time = draw_predictions(frame)
+        # Initialization of writer if None
+        if writer is None:
+            # initialize our video writer
+            fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+            #fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+            writer = cv2.VideoWriter(os.path.sep.join(["Output", args["file"]]),
+                                     fourcc, fps, (frame.shape[1], frame.shape[0]), True)
+            # some information on processing single frame
+            if total > 0:
+                print("[INFO] single frame took {:.4f} seconds".format(infer_time))
+                print("[INFO] estimated total time to finish: {:.4f}".format(
+                    infer_time * total))
+        # write the output frame to disk
+        writer.write(frame)
+    print("[INFO] cleaning up...")
+    writer.release()
+    cap.release()
+else:
+    print('Wrong input type')
