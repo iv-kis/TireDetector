25202ff585b040687cd82bf6a2257f1406fa6d27
ivkis <i.kisialiou@gmail.com>
2021-04-22 11:32:55 +0100

    latest tire_detector.py version

--- tire_detector.py
+++ tire_detector.py
@@ -5,6 +5,7 @@ import imutils
 
 import time
 import os
+import platform
 
 MODEL_PATH = 'model'  # folder name
 LABEL = 'tire'
@@ -17,21 +18,26 @@ TEXT_THICKNESS = 2
 TEXT_FONT = cv2.FONT_HERSHEY_SIMPLEX
 TEXT_COLOR = (255, 255, 255)
 
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", required=True, help="path to input image or video")
-ap.add_argument("-i", "--input", required=True, help="type of input: 'image' or 'video'")
-#ap.add_argument("-m", "--model", required=True, help="yolo4' or 'yolo4tiny'")
+ap.add_argument("-i", "--input", required=True, help="type of input: image / video")
+ap.add_argument("-m", "--model", required=True, help="yolo4 / yolo4tiny")
 ap.add_argument("-c", "--confidence", type=float, default=0.25, help="minimum probability to filter weak detections")
 ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applying non-maxima suppression")
 args = vars(ap.parse_args())
 
 # LOAD YOLO MODEL
-weightsPath = os.path.sep.join([MODEL_PATH, "custom-yolov4-tiny-detector_4000.weights"])
-configPath = os.path.sep.join([MODEL_PATH, "custom-yolov4-tiny-detector.cfg"])
+if args["model"]=='yolo4tiny':
+    weightsPath = os.path.sep.join([MODEL_PATH, "custom-yolov4-tiny-detector_4000.weights"])
+    configPath = os.path.sep.join([MODEL_PATH, "custom-yolov4-tiny-detector.cfg"])
+elif args["model"]=='yolo4':
+    weightsPath = os.path.sep.join([MODEL_PATH, "custom-yolov4-detector_3000.weights"])
+    configPath = os.path.sep.join([MODEL_PATH, "custom-yolov4-detector.cfg"])
+else:
+    raise Exception("Wrong model name. Must be yolo4 or yolo4tiny")
 print("[INFO] loading YOLO from disk...")
 model = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-# determine only the *output* layer names that we need from YOLO
+# determine only the output layer names that we need from YOLO
 layer_names = model.getLayerNames()
 layer_names = [layer_names[i[0] - 1] for i in model.getUnconnectedOutLayers()]
 
@@ -42,22 +48,16 @@ def draw_predictions(image):
     :return: image
     """
     (H, W) = image.shape[:2]  # input dimensions
-    # construct a blob from the input image and then perform a forward
-    # pass of the YOLO object detector, giving us our bounding boxes and
-    # associated probabilities
     blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
     model.setInput(blob)
     start = time.time()
     outputs = model.forward(layer_names)
     end = time.time()
-    # show timing information on YOLO
+    # show timing information
     infer_time = end - start
 
-    # initialize our lists of detected bounding boxes, confidences, and
-    # class IDs, respectively
     boxes = []
     confidences = []
-
     # loop over outputs
     for output in outputs:
         # loop over each of the detections
@@ -81,8 +81,7 @@ def draw_predictions(image):
     idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"], args["threshold"])
 
     # MAKE THE OUTPUT IMAGE
-    #caption_text = 'Model: {}'.format(model_name)
-    caption_text = ' No objects detected'
+    caption_text = ' Model: {}. No objects detected'.format(args['model'])
     if len(idxs) > 0:  # ensure at least one detection exists
         # loop over the indexes we are keeping
         for i in idxs.flatten():
@@ -103,7 +102,7 @@ def draw_predictions(image):
             cv2.putText(image, text,
                         (x, y + h + text_baseline + int(0.8 * text_height)),
                         TEXT_FONT, TEXT_SIZE, TEXT_COLOR, TEXT_THICKNESS)
-        caption_text = " Inference time: {:.1f} ms".format(infer_time * 1000)
+        caption_text = " Model: {}. Inference time: {:.1f} ms".format(args["model"], infer_time * 1000)
     # Caption
     imcaption = image.copy()
     (text_width, text_height), text_baseline = cv2.getTextSize(caption_text, TEXT_FONT, TEXT_SIZE, TEXT_THICKNESS)
@@ -115,19 +114,22 @@ def draw_predictions(image):
         
     return image, infer_time
 
+# Print system info
+print(platform.platform())
+print(platform.processor())
 
 # LOAD INPUT
 if args["input"] == 'image':
-    # python tire_detector.py -f car3.jpg -i image
+    # python tire_detector.py -f car3.jpg -i image -m yolo4
     image = cv2.imread(os.path.sep.join(["Input", args["file"]]))
     image, infer_time = draw_predictions(image)
     print("[INFO] YOLO inference took {:.6f} seconds".format(infer_time))
-    cv2.imwrite('Output/{}'.format(args["file"]), image)
+    cv2.imwrite(os.path.sep.join(['Output','{}_{}'.format(args["model"], args["file"])]), image)
     cv2.imshow("Output", image)
     cv2.waitKey(0)
 
 elif args["input"] == 'video':
-    # python tire_detector.py -f video1.mp4 -i video
+    # " Model: {}. Inference time: {:.1f} ms".format(args["model"], infer_time * 1000)
     cap = cv2.VideoCapture(os.path.sep.join(["Input", args["file"]]))
     fps = cap.get(cv2.CAP_PROP_FPS)
     writer = None
@@ -154,8 +156,11 @@ elif args["input"] == 'video':
             # initialize our video writer
             fourcc = cv2.VideoWriter_fourcc(*"mp4v")
             #fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-            writer = cv2.VideoWriter(os.path.sep.join(["Output", args["file"]]),
-                                     fourcc, fps, (frame.shape[1], frame.shape[0]), True)
+            writer = cv2.VideoWriter(os.path.sep.join(["Output",
+                                                       "{}_{}".format(args["model"], args["file"])
+                                                       ]),
+                                     fourcc, fps, (frame.shape[1], frame.shape[0]), True
+                                     )
             # some information on processing single frame
             if total > 0:
                 print("[INFO] single frame took {:.4f} seconds".format(infer_time))
@@ -167,4 +172,4 @@ elif args["input"] == 'video':
     writer.release()
     cap.release()
 else:
-    print('Wrong input type')
+    raise Exception('Wrong input type. Must be image or video')
